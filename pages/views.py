ef60f780d1339dfdf77d726293c5d87d41126fe4
esi <esi.taheri@yahoo.com>
2023-11-24 04:14:11 -0800

    add contact us & about us templates

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.views import generic
 from django.contrib import messages
+from accounts.models import CustomUser
+from django.http import HttpResponseRedirect
 
 
 from .forms import ContactUsFrom
@@ -13,6 +15,12 @@ class HomePageView(generic.TemplateView):
 class AboutUsPageView(generic.TemplateView):
     template_name = 'pages/about_us.html'
 
+    def get_context_data(self, **kwargs):
+        contex = super().get_context_data(**kwargs)
+        last_users = CustomUser.objects.all()[:4]
+        contex['last_users'] = last_users
+        return contex
+
 
 def contact_us_page_view(request):
     if request.method == 'POST':
@@ -21,6 +29,7 @@ def contact_us_page_view(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Your Message Successfully Saved.')
+            return HttpResponseRedirect(reverse('contact_us'))
 
     form = ContactUsFrom()
     return render(request, 'pages/contact_us.html', context={
